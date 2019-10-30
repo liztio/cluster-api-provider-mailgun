7b59dc12aa6dbc28117cf6fe9afa36cf238a9d50
liz <frostl@vmware.com>
2019-10-30 10:23:18 -0400

    wip

--- controllers/mailguncluster_controller.go
+++ controllers/mailguncluster_controller.go
@@ -17,8 +17,13 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 
 	"github.com/go-logr/logr"
+	"github.com/mailgun/mailgun-go"
+	"sigs.k8s.io/cluster-api/util"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -28,17 +33,48 @@ import (
 // MailgunClusterReconciler reconciles a MailgunCluster object
 type MailgunClusterReconciler struct {
 	client.Client
-	Log logr.Logger
+	Log       logr.Logger
+	Mailgun   mailgun.Mailgun
+	Recipient string
 }
 
 // +kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=mailgunclusters,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=mailgunclusters/status,verbs=get;update;patch
 
 func (r *MailgunClusterReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
+	ctx := context.Background()
 	_ = r.Log.WithValues("mailguncluster", req.NamespacedName)
 
-	// your logic here
+	var mgCluster infrastructurev1alpha3.MailgunCluster
+	if err := r.Get(ctx, req.NamespacedName, &mgCluster); err != nil {
+		// 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+		if apierrors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	cluster, err := util.GetOwnerMachine(ctx, r.Client, mgCluster.ObjectMeta)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if mgCluster.Status.MessageID != nil {
+		// We already sent a message, so skip reconcilation
+		return ctrl.Result{}, nil
+	}
+
+	subject := fmt.Sprintf("[%s] New Cluster %s requested", mgCluster.Spec.Priority, cluster.Name)
+	body := fmt.Sprint("Hello! One cluster please.\n\n%s\n", mgCluster.Spec.Request)
+
+	msg := mailgun.NewMessage(mgCluster.Spec.Requester, subject, body, r.Recipient)
+	_, msgID, err := r.Mailgun.Send(msg)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	mgCluster.Status.MessageID = &msgID
+	r.Update(ctx, &mgCluster)
 
 	return ctrl.Result{}, nil
 }
